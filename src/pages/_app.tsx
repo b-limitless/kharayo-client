0028db8b9f0480495d67773b7359c150a1097443
bharat shah <akashche72@gmail.com>
2021-08-01 19:16:13 +0400

    setting up urql provider in index.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,61 +1,11 @@
-import { ChakraProvider, ColorModeProvider } from '@chakra-ui/react'
-import { createClient, Provider, dedupExchange, fetchExchange } from 'urql';
-import { cacheExchange } from '@urql/exchange-graphcache';
-import { LoginMutation, LogoutMutation, MeDocument, MeQuery, RegisterMutation } from 'generated/graphql';
-import theme from '../theme'
+import { ChakraProvider, ColorModeProvider } from '@chakra-ui/react';
+import theme from '../theme';
 import './register.scss';
-import { betterUpdateQuery } from './betterUpdateQuery';
 
-const client = createClient({
-  url: 'http://localhost:4000/graphql',
-  exchanges: [dedupExchange, cacheExchange({
-    updates: {
-      Mutation: {
-        logout: (_result, args, cache, info) => {
-          betterUpdateQuery<LogoutMutation, MeQuery>(
-            cache,
-            {query: MeDocument},
-            _result,
-            (result, query) => ({me:null})
-          )
-        },
-        login: (_result, args, cache, info) => {
-          betterUpdateQuery<LoginMutation, MeQuery>(cache, {
-            query: MeDocument}, _result, 
-            (result, query) => {
-              if(result.login.errors) {
-                return query;
-              } else {
-                return {
-                  me: result.login.user
-                }
-              }
-            })
-        },
-        register: (_result, args, cache, info) => {
-          betterUpdateQuery<RegisterMutation, MeQuery>(cache, {
-            query: MeDocument}, _result, 
-            (result, query) => {
-              if(result.register.errors) {
-                return query;
-              } else {
-                return {
-                  me: result.register.user
-                }
-              }
-            })
-        }
-      }
-    }
-  }), fetchExchange],
-  fetchOptions: {
-    credentials: 'include'
-  }
-});
+
 
 function MyApp({ Component, pageProps }: any) {
   return (
-    <Provider value={client}>
     <ChakraProvider resetCSS theme={theme}>
       <ColorModeProvider
         options={{
@@ -65,7 +15,7 @@ function MyApp({ Component, pageProps }: any) {
         <Component {...pageProps} />
       </ColorModeProvider>
     </ChakraProvider>
-    </Provider>
+   
   )
 }
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import  NavBar  from "components/NavBar"
-
+import { withUrqlClient } from 'next-urql';
+import { createUrqlClient } from "utils/createUrqlClient";
 const Index = () => (
  <div>
      <NavBar></NavBar>
@@ -7,4 +8,4 @@ const Index = () => (
  </div>
 )
 
-export default Index
+export default withUrqlClient(createUrqlClient)(Index);
